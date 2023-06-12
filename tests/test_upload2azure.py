85ad3fe074777a5c1a58e8d75793c461c70a5b31
Lode Nachtergaele <you@example.comlode.nachtergaele@gmail.com>
2023-06-12 12:14:49 +0200

    test for zip_dir

--- tests/test_upload2azure.py
+++ tests/test_upload2azure.py
@@ -1,5 +1,27 @@
-from src import upload2azure
+import pytest
+
+from src.upload2azure import getLogger, zip_dir
+
+
+@pytest.fixture(scope="session")
+def template_dir(tmp_path_factory):
+    template_dir = tmp_path_factory.mktemp("test_dir")
+    zip_dir = template_dir / "07062023"
+    zip_dir.mkdir()
+    (zip_dir / "test1.txt").write_text("test, line, one", "utf-8")
+    (zip_dir / "test2.txt").write_text("test, line, two", "utf-8")
+    return template_dir
 
 
 def test_getLogger():
-    assert upload2azure.getLogger()
+    assert getLogger()
+
+
+def test_zip_dir(template_dir):
+    assert template_dir.exists()
+    assert (template_dir / "07062023").exists()
+    assert (template_dir / "07062023" / "test1.txt").exists()
+    assert (template_dir / "07062023" / "test2.txt").exists()
+    logger = getLogger()
+    zip_dir(template_dir / "07062023", logger)
+    assert (template_dir / "07062023.zip").exists()
